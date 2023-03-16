f5ddb0cde04a4501fed93d1091a2ea173d8dcb11
Maxim Isaev <isaev_0602@mail.ru>
2023-03-16 21:03:59 +0500

    workflow and isort changes

--- api_yamdb/api/v1/urls.py
+++ api_yamdb/api/v1/urls.py
@@ -2,8 +2,8 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
-                    ReviewViewSet, signup_view, TitleViewSet, token_view,
-                    UsersViewSet)
+                    ReviewViewSet, TitleViewSet, UsersViewSet, signup_view,
+                    token_view)
 
 router_v1 = DefaultRouter()
 
